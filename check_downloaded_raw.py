8b96b1db1ba0ba2e3ebcde52d9d23e4f578710c0
kawaiigamer <kawaiigamer@gmail.com>
2016-03-11 04:51:48 +0900

    Create check_downloaded_raw.py

---
+++ check_downloaded_raw.py
@@ -0,0 +1,23 @@
+import os,glob
+
+def dirs(d):
+    return next(os.walk(d))[1]
+
+def dirc(d):
+    return len([f for f in os.listdir(d)if os.path.isfile(os.path.join(d, f))])
+
+print("ckdir v1.0")
+d=dirs('.')
+for i in range(0,len(d)): print ('('+str(i)+')',d[i]) 
+while True:
+             try:
+                s=input('_->')
+                w=d[int(s)]
+             except Exception as e:
+                 continue
+             break
+t=dirs(w)
+print("folders with file count < 15 :")
+for vol in t:
+    if dirc(w+'/'+vol) < 15:
+        print('|',vol)
